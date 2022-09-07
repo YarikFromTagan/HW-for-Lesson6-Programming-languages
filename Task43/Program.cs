f189bd67ab72be92b006783bc5bd93651a19c595
Yar_Sanych_NB <y.krivoff@gmail.com>
2022-09-07 15:32:56 +0300

    add Task43

---
+++ Task43/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку
+пересечения двух прямых, заданных уравнениями
+y = k1 * x + b1,
+y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.
+Example:
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+Console.Clear();
+Console.WriteLine("Для прямой, заданной уравнением y = k1 * x + b1");
+Console.Write("введите b1 = ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите k1 = ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.WriteLine("Для прямой, заданной уравнением y = k2 * x + b2");
+Console.Write("введите b2 = ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите k2 = ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+if(k1 == k2)
+{
+    Console.WriteLine("Заданные Вами прямые не имеют точки пересечения, т.е. они параллельны!!!");
+    Console.WriteLine();
+    System.Environment.Exit(0);
+}
+
+double Xresult = (b2 - b1) / (k1 - k2);
+double Yresult = k1 * Xresult + b1;
+
+Console.WriteLine($"Координаты точки пересечения заданных Вами прямых A({Xresult}; {Yresult})");
+Console.WriteLine();
